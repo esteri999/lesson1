5ed5ceeede371b433cefd94743df42a704f0663c
EsteriMikaberidze <154770025+esteri999@users.noreply.github.com>
2024-07-31 18:34:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let a = 100;
+
+if (a < 50) {
+    console.log(a < 50);
+}
+else if (a > 20) {
+    console.log(a > 20);
+}
+else if (a > 50 || a < 20 ) {
+    console.log("error");
+}
+
+let username = "mariami";
+if ( username="mariami") {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+
+
+let username2 = "lika" ? 'true' : 'false';
+ 
+console.log(username2);
+
